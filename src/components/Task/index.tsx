53355f8d3a3306e3687514cbc0ffbc96af4f89ba
Vitor Sergio <vitorsergio111@gmail.com>
2023-10-30 23:39:34 -0300

    feat: delete tasks func + change done number func

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,27 +1,64 @@
 import { styles } from "./styles";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { useState } from "react";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { Alert } from "react-native";
+import { TaskType } from "../../types/task";
 
 type Props = {
-  description: string;
+  task: TaskType;
+  setTasks: React.Dispatch<React.SetStateAction<TaskType[]>>;
 };
 
-export function Task({ description }: Props) {
+export function Task({ task, setTasks }: Props) {
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
-  console.log(toggleCheckBox);
+
+  function handleRemoveTask() {
+    Alert.alert("Delete", "Are you sure you want to delete this task?", [
+      {
+        text: "Yes",
+        onPress: () =>
+          setTasks((prevTasks) => prevTasks.filter((item) => item != task)),
+      },
+      {
+        text: "No",
+        style: "cancel",
+      },
+    ]);
+  }
+
+  function handleTaskChecked(isChecked: boolean) {
+    setTasks((prevTasks) =>
+      prevTasks.map((item) =>
+        item.id == task.id ? { ...item, done: isChecked } : item
+      )
+    );
+    setToggleCheckBox(isChecked);
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.content}>
         <BouncyCheckbox
-          onPress={(isChecked) => setToggleCheckBox(isChecked)}
+          onPress={(isChecked) => handleTaskChecked(isChecked)}
           style={styles.checkBox}
           size={18}
           fillColor="#5E60CE"
         />
-        <Text style={styles.description}>{description}</Text>
-        <Image source={require("../../../assets/trash.png")} />
+        <Text
+          style={[
+            styles.description,
+            toggleCheckBox && {
+              textDecorationLine: "line-through",
+              color: "#808080",
+            },
+          ]}
+        >
+          {task.description}
+        </Text>
+        <TouchableOpacity onPress={handleRemoveTask}>
+          <Image source={require("../../../assets/trash.png")} />
+        </TouchableOpacity>
       </View>
     </View>
   );
