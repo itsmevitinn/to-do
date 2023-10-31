d29ce693035c60e55c5fe647202b71b1111e4525
Vitor Sergio <vitorsergio111@gmail.com>
2023-10-30 23:40:00 -0300

    refactor: showing done value dinamically

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -4,14 +4,15 @@ import { Text, View } from "react-native";
 type Props = {
   name: string;
   textColor: string;
+  quantity: number;
 };
 
-export function Counter({ name, textColor }: Props) {
+export function Counter({ name, textColor, quantity }: Props) {
   return (
     <View style={styles.container}>
       <Text style={[styles.text, { color: textColor }]}>{name}</Text>
       <View style={styles.numberView}>
-        <Text style={styles.numberText}>0</Text>
+        <Text style={styles.numberText}>{quantity}</Text>
       </View>
     </View>
   );
