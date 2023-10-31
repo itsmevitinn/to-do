f819dc0f5acc9e94858f83312ecebc107fda09bf
Vitor Sergio <vitorsergio111@gmail.com>
2023-10-30 23:42:46 -0300

    feat: using task type at tasks

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,19 +4,16 @@ import { Header } from "../../components/Header";
 import { useState } from "react";
 import { CreateTask } from "../../components/CreateTask";
 import { DisplayTasks } from "../../components/DisplayTasks";
+import { TaskType } from "../../types/task";
 
 export function Home() {
-  const [tasks, setTasks] = useState<string[]>([]);
-
-  function handleTaskAdd(newTask: string) {
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-  }
+  const [tasks, setTasks] = useState<TaskType[]>([]);
 
   return (
     <View style={styles.container}>
       <Header />
       <CreateTask setTasks={setTasks} />
-      <DisplayTasks tasks={tasks} />
+      <DisplayTasks tasks={tasks} setTasks={setTasks} />
     </View>
   );
 }
