6bbd4b44df885e3b71234aeadf1834793ff03fc2
Vitor Sergio <vitorsergio111@gmail.com>
2023-10-31 00:24:42 -0300

    feat: loading fonts before entire app

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
-import { StatusBar } from "react-native";
+import { StatusBar, View } from "react-native";
 import { Home } from "./src/screens/Home";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
+    "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <Home />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent={true}
       />
-    </>
+    </View>
   );
 }
