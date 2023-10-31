9a682d404ac5a7fc74caa73edc721bc09737a211
Vitor Sergio <vitorsergio111@gmail.com>
2023-10-30 23:40:42 -0300

    refactor: creating task accordingly with the new task type

--- src/components/CreateTask/index.tsx
+++ src/components/CreateTask/index.tsx
@@ -1,26 +1,34 @@
 import { styles } from "./styles";
 import { View, TextInput, TouchableOpacity, Image } from "react-native";
 import { useState } from "react";
+import { Alert } from "react-native";
+import { TaskType } from "../../types/task";
 
 type Props = {
-  setTasks: React.Dispatch<React.SetStateAction<string[]>>;
+  setTasks: React.Dispatch<React.SetStateAction<TaskType[]>>;
 };
 
 export function CreateTask({ setTasks }: Props) {
-  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
   const [isFocused, setIsFocused] = useState(false);
 
-  function handleTaskAdd(newTask: string) {
-    setTasks((prevTasks) => [...prevTasks, task]);
-    setTask("");
+  function handleTaskAdd(description: string) {
+    if (description == "") {
+      return Alert.alert("You can't add an empty task!");
+    }
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: prevTasks.length, description: description, done: false },
+    ]);
+    setDescription("");
   }
 
   return (
     <View style={styles.container}>
       <TextInput
         placeholderTextColor="#808080"
-        value={task}
-        onChangeText={setTask}
+        value={description}
+        onChangeText={setDescription}
         style={[styles.input, isFocused && { borderColor: "#5E60CE" }]}
         placeholder="Add a new task"
         onBlur={() => setIsFocused(false)}
@@ -28,9 +36,9 @@ export function CreateTask({ setTasks }: Props) {
       ></TextInput>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => handleTaskAdd(task)}
+        onPress={() => handleTaskAdd(description)}
       >
-        <Image alt="Plus button" source={require("../../../assets/plus.png")} />
+        <Image alt="Plus icon" source={require("../../../assets/plus.png")} />
       </TouchableOpacity>
     </View>
   );
