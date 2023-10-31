bdee22b93854ee29a2f47013f0a489222afde6e2
Vitor Sergio <vitorsergio111@gmail.com>
2023-10-30 23:42:07 -0300

    refactor: send quantity to Counter component

--- src/components/DisplayTasks/index.tsx
+++ src/components/DisplayTasks/index.tsx
@@ -1,26 +1,33 @@
 import { styles } from "./styles";
-import { FlatList, View, Text, Image } from "react-native";
+import { FlatList, View } from "react-native";
 import { Counter } from "../Counter";
 import { EmptyComponent } from "../EmptyComponent";
+import { useState } from "react";
 import { Task } from "../Task";
+import { TaskType } from "../../types/task";
 
 type Props = {
-  tasks: string[];
+  tasks: TaskType[];
+  setTasks: React.Dispatch<React.SetStateAction<TaskType[]>>;
 };
 
-export function DisplayTasks({ tasks }: Props) {
+export function DisplayTasks({ tasks, setTasks }: Props) {
   return (
     <View style={styles.container}>
       <View style={styles.info}>
-        <Counter name="Created" textColor="#4EA8DE" />
-        <Counter name="Done" textColor="#8284FA" />
+        <Counter name="Created" textColor="#4EA8DE" quantity={tasks.length} />
+        <Counter
+          name="Done"
+          textColor="#8284FA"
+          quantity={tasks.filter((task) => task.done == true).length}
+        />
       </View>
       <FlatList
         style={{ marginBottom: 32 }}
         data={tasks}
         ListEmptyComponent={EmptyComponent}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <Task description={item} />}
+        renderItem={({ item }) => <Task task={item} setTasks={setTasks} />}
       />
     </View>
   );
